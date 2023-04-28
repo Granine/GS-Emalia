80d62bcb85338e77e149d9d510bee0a1574f7110
Granine <guanzheng.huang@hotmail.com>
2023-04-27 23:51:26 -0700

    Include integration test template

--- emalia_test/integration_test/emalia_integration_test.py
+++ emalia_test/integration_test/emalia_integration_test.py
@@ -1,1 +1,8 @@
 import pytest
+import sys
+sys.path.append(f"{__file__}/../../../emalia_src")
+import emalia_main as emalia
+
+def test_emalia_basic():
+    emanager = emalia.EmailManager()
+    assert emanager
