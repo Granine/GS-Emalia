92531076800a7993ec89ce3894b72bd80df96323
Granine <guanzheng.huang@hotmail.com>
2023-04-30 00:58:04 -0700

    Minor reformatting to test template

--- emalia_test/unit_test/EmailManger_test.py
+++ emalia_test/unit_test/EmailManger_test.py
@@ -8,7 +8,7 @@ import smtplib
 def test_EmailManager_basic():
         # Create a MagicMock object that returns "abc" when its read method is called
     mock_s = mock.MagicMock(spec=smtplib.SMTP_SSL)
-    mock_s.login.return_value = "abc"
+    mock_s.login.return_value = "value"
 
     # Implement the __enter__ and __exit__ methods to support the context management protocol
     mock_s.__enter__.return_value = mock_s
@@ -18,4 +18,3 @@ def test_EmailManager_basic():
     with mock.patch('smtplib.SMTP_SSL', return_value=mock_s) as mock_smtp:
         emanager = EmailManager.EmailManager()
         assert emanager
-        assert False
