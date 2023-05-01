054cfff8b75d1b409899645ded11c2bac1edf6d4
Granine <guanzheng.huang@hotmail.com>
2023-05-01 01:25:59 -0700

    EMail Manger test update

--- emalia_test/unit_test/EmailManger_test.py
+++ emalia_test/unit_test/EmailManger_test.py
@@ -4,17 +4,30 @@ from unittest import mock
 sys.path.append(f"{__file__}/../../../emalia_src")
 import EmailManager
 import smtplib
+import imaplib
+from email.parser import BytesParser
+from email.policy import default
 
-def test_EmailManager_basic():
-        # Create a MagicMock object that returns "abc" when its read method is called
-    mock_s = mock.MagicMock(spec=smtplib.SMTP_SSL)
-    mock_s.login.return_value = "value"
-
+def test_EmailManager_basic_init():
+    # mock smtp
+    mock_smtp_value = mock.MagicMock(spec=smtplib.SMTP_SSL)
+    mock_smtp_value.login.return_value = "True"
+    mock_smtp_value.send_message.return_value = "Complete"
     # Implement the __enter__ and __exit__ methods to support the context management protocol
-    mock_s.__enter__.return_value = mock_s
-    mock_s.__exit__.return_value = None
-
-    # Replace smtplib.SMTP_SSL with the MagicMock object using patch
-    with mock.patch('smtplib.SMTP_SSL', return_value=mock_s) as mock_smtp:
-        emanager = EmailManager.EmailManager()
-        assert emanager
+    mock_smtp_value.__enter__.return_value = mock_smtp_value
+    mock_smtp_value.__exit__.return_value = None
+    
+    # mock imap
+    mock_imap_value = mock.MagicMock(spec=imaplib.IMAP4_SSL)
+    mock_imap_value.login.return_value = "True"
+    # Implement the __enter__ and __exit__ methods to support the context management protocol
+    mock_imap_value.__enter__.return_value = mock_imap_value
+    mock_imap_value.__exit__.return_value = None
+    mock_select_response = ""
+    mock_imap_value.select.return_value = mock_select_response
+    mock_search_response = ('OK', [b'1 2 3'])
+    mock_imap_value.search.return_value = mock_search_response
+    mock_fetch_response = ('OK', [(b'1', b'SOME_DATA')])
+    mock_imap_value.fetch.return_value = mock_fetch_response
+    
+    
