059be0c2d8336d64e91020bdaf2580b419e1d9f2
Granine <guanzheng.huang@hotmail.com>
2023-04-30 21:21:10 -0700

    WIP for email manager read email parsing

--- emalia_src/EmailManager.py
+++ emalia_src/EmailManager.py
@@ -3,6 +3,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.parser import BytesParser
 from email.policy import default
+from email.message import Message
 import os
 
 # Set up IMAP connection to read emails
@@ -74,25 +75,98 @@ class EmailManager():
         """ Fetch unread emails by count number
         @param `count:int` Number of latest unread to fetch, <0 for all
         @param `mark_read:bool` if true, mark fetched email as "\seen"
-        @return `:list` return a list of email fetched. Format: [[unread_email_id, unread_email_status, unread_email_content, unread_email raw]]
+        @return `:list` return a list of email fetched. Format: [[unread_email_id, unread_email_status, email, unread_email raw]]
         """
-
-        with imaplib.IMAP4_SSL(self.HANDLER_IMAP) as imap:
+        with imaplib.IMAP4_SSL(**self.HANDLER_IMAP) as imap:
             imap.login(self.HANDLER_EMAIL, self.HANDLER_PASSWORD)
-            imap.select('inbox')
+            imap.select("inbox")
 
             # Search for all unread emails
-            status, response = imap.search(None, 'UNSEEN')
+            status, response = imap.search(None, "UNSEEN")
             # return based on count number
-            unread_emails_id = response[0].split()[:count] if count > len(response[0].split()) else response[0].split()
+            unread_emails_ids = response[0].split()[count:] if count < len(response[0].split()) else response[0].split()
             unread_emails_list = []
             # fetch and record each email by ID
-            for unread_email_id in unread_emails_id:
-                unread_email_status, unread_email = imap.fetch(unread_email_id, '(BODY[HEADER.FIELDS (FROM SUBJECT DATE)] BODY[TEXT])')
-                raw_email = response[0][1]
-                unread_email_content = BytesParser(policy=default).parsebytes(raw_email)
-                unread_emails_list.append([unread_email_id, unread_email_status, unread_email_content, unread_email])
+            for unread_email_id in unread_emails_ids:
+                unread_email_status, unread_email = imap.fetch(unread_email_id, "(BODY.PEEK[HEADER.FIELDS (FROM SUBJECT DATE)] BODY.PEEK[TEXT])")
+                raw_email = unread_email[0][1]
+                #!
+                email = BytesParser(policy=default).parsebytes(raw_email)
+                unread_emails_list.append([unread_email_id, unread_email_status, email, unread_email])
+                # for somereason BODY.PEEK[] does not work, so label as seen/unseen
                 if mark_read:
                     # Mark the email as read
                     imap.store(unread_email_id, "+FLAGS", "\\Seen")
+                else:
+                    imap.store(unread_email_id, "-FLAGS", "\\Seen")
         return unread_emails_list
+    
+    def mark_email(self, target:int|str, action:str="+FLAGS", flag:str="\\Seen")->list[Message]:
+        """ Fetch unread emails by count number
+        @param `count:int` Number of latest unread to fetch, <0 for all
+        @param `mark_read:bool` if true, mark fetched email as "\seen"
+        @return `:list` return a list of email fetched. Format: [[unread_email_id, unread_email_status, email, unread_email raw]]
+        """
+        if action.lower() == "add":
+            action = "+FLAGS"
+        elif action.lower() == "remove":
+            action = "+FLAGS"
+        elif action.lower() == "replace":
+            action = "FLAGS"
+        
+        with imaplib.IMAP4_SSL(**self.HANDLER_IMAP) as imap:
+            imap.login(self.HANDLER_EMAIL, self.HANDLER_PASSWORD)
+            imap.select("inbox")
+
+            # Search for all unread emails
+            status, response = imap.search(None, "ALL")
+            # return based on count number
+            unread_emails_ids = response[0].split()[target:] if target < len(response[0].split()) else response[0].split()
+            unread_emails_list = []
+            # fetch and record each email by ID
+            for unread_email_id in unread_emails_ids:
+                    imap.store(unread_email_id, action, flag)
+    
+    
+    
+    def parse_email(self, email:Message):
+        def clean(text):
+        # clean text for creating a folder
+            return "".join(c if c.isalnum() else "_" for c in text)
+        subject = email["Subject"]
+        body = ""
+        if email.is_multipart():
+            for part in email.walk():
+                content_type = part.get_content_type()
+                content_disposition = part.get("Content-Disposition", None)
+                if content_type == "text/plain" and "attachment" not in content_disposition:
+                    body = part.get_payload(decode=True).decode()
+                elif content_disposition is not None and content_disposition.strip().startswith("attachment"):
+                    file_data = part.get_payload(decode=True)
+                    file_name = part.get_filename()
+                    attachments.append((file_name, file_data))
+                    
+                    if file_name:
+                        folder_name = clean(subject)
+                        if not os.path.isdir(folder_name):
+                            # make a folder for this email (named after the subject)
+                            os.mkdir(folder_name)
+                        filepath = os.path.join(folder_name, file_name)
+                        # download attachment and save it
+                        open(filepath, "wb").write(part.get_payload(decode=True))
+        else:
+            body = email.get_payload(decode=True).decode()
+
+        # Get the attachments
+        attachments = []
+        if email.is_multipart():
+            for part in email.walk():
+                content_disposition = part.get("Content-Disposition", None)
+            
+        print("Subject:", subject)
+        print("Body:", body)
+
+
+a = EmailManager()
+print(a_r:=a.fetch_unread_email(2, False))
+a.parse_email(a_r[0][2])
