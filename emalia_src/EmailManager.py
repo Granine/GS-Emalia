a0604b4921e05f03df9b63f4ded913e291182750
Granine <guanzheng.huang@hotmail.com>
2023-05-02 00:05:24 -0700

    EmailManger Bug fix and mark function

--- emalia_src/EmailManager.py
+++ emalia_src/EmailManager.py
@@ -4,7 +4,9 @@ from email.mime.text import MIMEText
 from email.parser import BytesParser
 from email.policy import default
 from email.message import Message
+from email import message_from_bytes
 import os
+import pathlib
 
 # Set up IMAP connection to read emails
 class EmailManager(): 
@@ -14,7 +16,7 @@ class EmailManager():
     TODO: support saving attachments
     TODO: support common smtp and imap
     """
-    def __init__(self, enable_history:bool=True, HANDLER_EMAIL:str="", HANDLER_PASSWORD:str="", HANDLER_SMTP:str|dict="smtp.gmail.com", HANDLER_IMAP:str|dict="imap.gmail.com"):
+    def __init__(self, enable_history:bool=True, attachment_path:str="", HANDLER_EMAIL:str="", HANDLER_PASSWORD:str="", HANDLER_SMTP:str|dict="smtp.gmail.com", HANDLER_IMAP:str|dict="imap.gmail.com"):
         """initialize email manager service
         TODO @param `enable_history:str` if not False will record email sent and received, takes "local", [FILE PATH], "cache", "cache-[Int]" and "all"
           if local: save to a local file that can be accessed later at default location __file__/..
@@ -22,6 +24,7 @@ class EmailManager():
           if cache: email in variable, unlimited size list from earliest to latest
           if cache-[INT]: save a max of INT email in cache, then delete from latest 
           if all, [FILE PATH]-[INT]: save to both variable and file
+        @param `attachment_path:str` path to save attachments, attachment stored in folder named subject+id, default saved in par as EmailManager
         @param `HANDLER_EMAIL:str` email address (also login email to smtp and imap), if not provided, attempt to read from environmental var
         @param `HANDLER_PASSWORD:str` login password, if not provided, attempt to read from environmental var
         @param `HANDLER_SMTP:str|dict` smtp server configuration, str for server address, dict for elements supported by smtplib.SMTP_SSL, enter None or "" or 0 to read from Environmental variable
@@ -31,6 +34,8 @@ class EmailManager():
         assert self.HANDLER_EMAIL and isinstance(self.HANDLER_EMAIL, str)
         self.HANDLER_PASSWORD = HANDLER_PASSWORD if HANDLER_PASSWORD else os.environ.get("HANDLER_PASSWORD")
         assert self.HANDLER_PASSWORD and isinstance(self.HANDLER_PASSWORD, str)
+        self.attachment_path = pathlib.Path(attachment_path).resolve() if attachment_path else pathlib.Path( __file__ + "/..").resolve()
+        assert self.attachment_path.exists()
         # SMTP
         if HANDLER_SMTP and isinstance(HANDLER_SMTP, str):
             self.HANDLER_SMTP = {"host": HANDLER_SMTP, "port": 465}
@@ -82,17 +87,22 @@ class EmailManager():
             imap.select("inbox")
 
             # Search for all unread emails
-            status, response = imap.search(None, "UNSEEN")
+            search_status, response = imap.search(None, "UNSEEN")
+            if search_status.lower() != "ok":
+                raise ConnectionError(f"Cannot perform search")
             # return based on count number
-            unread_emails_ids = response[0].split()[count:] if count < len(response[0].split()) else response[0].split()
+            unread_emails_ids = response[0].split()[-count:] if count < len(response[0].split()) else response[0].split()
             unread_emails_list = []
             # fetch and record each email by ID
             for unread_email_id in unread_emails_ids:
-                unread_email_status, unread_email = imap.fetch(unread_email_id, "(BODY.PEEK[HEADER.FIELDS (FROM SUBJECT DATE)] BODY.PEEK[TEXT])")
+                unread_email_id = unread_email_id.decode()
+                unread_email_status, unread_email = imap.fetch(unread_email_id, "(RFC822)")
+                if unread_email_status.lower() != "ok":
+                    raise ConnectionError(f"Cannot fetch email {unread_email_id}: {unread_email}")
                 raw_email = unread_email[0][1]
-                #!
                 email = BytesParser(policy=default).parsebytes(raw_email)
-                unread_emails_list.append([unread_email_id, unread_email_status, email, unread_email])
+                email = message_from_bytes(raw_email)
+                unread_emails_list.append([unread_email_id, email])
                 # for somereason BODY.PEEK[] does not work, so label as seen/unseen
                 if mark_read:
                     # Mark the email as read
@@ -101,11 +111,15 @@ class EmailManager():
                     imap.store(unread_email_id, "-FLAGS", "\\Seen")
         return unread_emails_list
     
-    def mark_email(self, target:int|str, action:str="+FLAGS", flag:str="\\Seen")->list[Message]:
-        """ Fetch unread emails by count number
-        @param `count:int` Number of latest unread to fetch, <0 for all
-        @param `mark_read:bool` if true, mark fetched email as "\seen"
-        @return `:list` return a list of email fetched. Format: [[unread_email_id, unread_email_status, email, unread_email raw]]
+    def mark_email(self, target:int|list|str, action:str="+FLAGS", flag:str="\\Seen")->list[str]:
+        """  Mark target emails with flag based on action
+        @param `target:int|list|str`
+          if int: the x most recent email to label in inbox
+          if list of email ids: mark each id listed
+          if str: mark the email in target
+        @param `action:str` "add", "+FLAG", "remove", "-FLAG", "replace", "FLAGS"
+        @param  `flag:str` tag to mark, must be supported tags, for example "\\Seen"
+        @return `:list` return a list of email ids marked
         """
         if action.lower() == "add":
             action = "+FLAGS"
@@ -118,28 +132,38 @@ class EmailManager():
             imap.login(self.HANDLER_EMAIL, self.HANDLER_PASSWORD)
             imap.select("inbox")
 
-            # Search for all unread emails
-            status, response = imap.search(None, "ALL")
-            # return based on count number
-            unread_emails_ids = response[0].split()[target:] if target < len(response[0].split()) else response[0].split()
-            unread_emails_list = []
+            if isinstance(target, int):
+                status, response = imap.search(None, "ALL")
+                emails_ids = response[0].split()[-target:] if target < len(response[0].split()) else response[0].split()
+            elif (isinstance(target, list) or isinstance(target, tuple)) and target:
+                emails_ids = target
+            elif isinstance(target, str) and target:
+                emails_ids = [target]
+            else:
+                raise AttributeError("Unknown target")
+            emails_list = []
+            
             # fetch and record each email by ID
-            for unread_email_id in unread_emails_ids:
-                    imap.store(unread_email_id, action, flag)
-    
-    
+            for email_id in emails_ids:
+                if isinstance(email_id, bytes):
+                    emails_list.append(email_id.decode())
+                elif isinstance(email_id, str):
+                    emails_list.append(email_id)
+                else:
+                    raise AttributeError("Unknown email ID type")
+                imap.store(email_id, action, flag)
+        return emails_list
     
     def parse_email(self, email:Message):
         def clean(text):
         # clean text for creating a folder
             return "".join(c if c.isalnum() else "_" for c in text)
-        subject = email["Subject"]
         body = ""
         if email.is_multipart():
             for part in email.walk():
                 content_type = part.get_content_type()
                 content_disposition = part.get("Content-Disposition", None)
-                if content_type == "text/plain" and "attachment" not in content_disposition:
+                if content_type == "text/plain" and "attachment" not in str(content_disposition):
                     body = part.get_payload(decode=True).decode()
                 elif content_disposition is not None and content_disposition.strip().startswith("attachment"):
                     file_data = part.get_payload(decode=True)
@@ -147,7 +171,7 @@ class EmailManager():
                     attachments.append((file_name, file_data))
                     
                     if file_name:
-                        folder_name = clean(subject)
+                        folder_name = self.attachment_path
                         if not os.path.isdir(folder_name):
                             # make a folder for this email (named after the subject)
                             os.mkdir(folder_name)
@@ -163,10 +187,17 @@ class EmailManager():
             for part in email.walk():
                 content_disposition = part.get("Content-Disposition", None)
             
-        print("Subject:", subject)
         print("Body:", body)
-
-
-a = EmailManager()
-print(a_r:=a.fetch_unread_email(2, False))
-a.parse_email(a_r[0][2])
+        print(email.values())
+        return {
+            "Body": body, 
+            "Return-Path": email["Return-Path"], 
+            "Received": email["Received"], 
+            "Date": email["Date"],  
+            "From": email["From"],  
+            "Subject": email["Subject"],  
+            "Sender": email["Sender"],
+            "To": email["To"], 
+            "cc": email["cc"]
+        }
+        
