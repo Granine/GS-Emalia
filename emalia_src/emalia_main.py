7f954c75405762641d87ffa320181eac5b368de5
Granine <guanzheng.huang@hotmail.com>
2023-05-03 00:39:29 -0700

    Emalis main comment and todo update, new scan_inv

--- emalia_src/emalia_main.py
+++ emalia_src/emalia_main.py
@@ -17,9 +17,9 @@ Standard mode
 """
 
 class Emalia():
+    instance_name = "Emalia" # name of the service robot, Emalia is her default name
     def __init__(self, permission:str="default", HANDLER_EMAIL:str="", HANDLER_PASSWORD:str="", HANDLER_SMTP:str|dict="smtp.gmail.com", HANDLER_IMAP:str|dict="imap.gmail.com"):
-        """Create a email service bot that start handling incoming emails
-
+        """Create a email service robot instance. Make sure you run mainloop to start service
         @param permission (str, optional): What Emalia is allowed to do to local file
             {"action": ACTION, range": RANGE}
                 ACTION(str|list): "read", "write", "shell", "all" what Emalia is allowed to do with data
@@ -33,6 +33,7 @@ class Emalia():
         @param HANDLER_PASSWORD (str|optional): will attempt to read from env var if empty or not provided
         @param HANDLER_SMTP:str|dict SMTP default supports gmail, will attempt to read from env var if empty
         @param HANDLER_IMAP:str|dict IMAP default supports gmail, will attempt to read from env var if empty
+        
         """
         if isinstance(permission, str):
             if permission.lower() == "default":
@@ -46,10 +47,16 @@ class Emalia():
         
         self.email_handler = EmailManager.EmailManager(HANDLER_PASSWORD=HANDLER_PASSWORD, HANDLER_EMAIL=HANDLER_EMAIL, HANDLER_SMTP=HANDLER_SMTP, HANDLER_IMAP=HANDLER_IMAP)
         
-    def main_loop(self):
+    def main_loop(self, scan_interval:float=5):
+        """Start the email listener
+        @param `scan_interval:float` the time to pause between each email scan session, if processing tie (request time >= scan_interval, there will be no pause)
+        Info: Only one main_loop or async_main_loop can run, all other calls will not create new Emalia loops. Please create new Emalia Object to do such task
+        """
+        # TODO check running
+        # TODO enforce time between scan
         self.running = True
         while self.running:
-            time.sleep(2)
+            time.sleep(scan_interval)
             print(self.email_handler.fetch_unread_email(1, False))
             pass
         
