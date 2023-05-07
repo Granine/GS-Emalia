c53c1b4522cd70d8b420c8a2dbd535eafe0ad629
Granine <guanzheng.huang@hotmail.com>
2023-05-07 01:02:31 -0700

    Added function template and class comment

--- emalia_src/emalia_main.py
+++ emalia_src/emalia_main.py
@@ -19,11 +19,30 @@ Standard mode
 """
 
 class Emalia():
+    """an email interacted system that manages and perform a list of predefined tasks.
+    Basic:
+        - can set different security levels for each task (free, restricted(can set multiple password or let emalia remember vip sender emails), locked)
+        - can set a range files to access, support directory depth control, whitelist and blacklist
+        - conversation through email and reply
+    Receive Email Structure:
+        body: [action] [command(s)] [password] + [attachments]
+    Supported tasks:
+        0. manage emalia: [Emalia Instance Name]/0 [command]: various command to manage emalia
+        1. read file: READ/1 [PATH]: zip if directory
+        2. write file: WRITE/2 [(optional)PATH to directory] + attachment list: write all to a directory (auto create if DNE)
+        3. make request: REQUEST/3 [Method] // [URL] // [HEADER] // [BODY]: enter None for a field that is not needed, result will be returned
+        4. execute powershell: SHELL/POWERSHELL/4 [command]: (DANGER) run powershell command
+        5. execute python: PYTHON/5 [code]: (DANGER) run python in-process
+        
+        9. custom tasks: CUSTOM/9 [task]: store custom tasks, one can run with their custom command
+
+    """
     instance_name:str = "Emalia" # name of the service robot, Emalia is her default name
     server_start_time:datetime = None # tracks the start time of last server
     server_running:bool = False # if True, a server is running, set to false will stop server at next loop
     max_send_count = 0 # max email emalia can send per instance, <0 for infinite
     statistics:dict = {"sent": None, "received": None} # track statistics for current/last running instance, {"sent":int, "received":int}
+    
     def __init__(self, permission:str="default", HANDLER_EMAIL:str="", HANDLER_PASSWORD:str="", HANDLER_SMTP:str|dict="smtp.gmail.com", HANDLER_IMAP:str|dict="imap.gmail.com"):
         """Create a email service robot instance. Make sure you run mainloop to start service
         @param permission (str, optional): What Emalia is allowed to do to local file
@@ -40,7 +59,6 @@ class Emalia():
         @param HANDLER_PASSWORD (str|optional): will attempt to read from env var if empty or not provided
         @param HANDLER_SMTP:str|dict SMTP default supports gmail, will attempt to read from env var if empty
         @param HANDLER_IMAP:str|dict IMAP default supports gmail, will attempt to read from env var if empty
-        
         """
         if isinstance(permission, str):
             if permission.lower() == "default":
@@ -91,6 +109,49 @@ class Emalia():
         Repeated call have no effect"""
         self.server_running = False
         
+    def _validate_password(password):
+        """Check if a user provided password is the save as record
+        """
+        pass
+        
+    def _action_manage_emalia(self, emalia_command):
+        """0 Alter emalia behaviour by emalia permission
+        """
+        pass
+        
+    def _action_read_file(self, path:str):
+        """1 read the content of a file by emalia permission
+        """
+        pass
+    
+    def _action_write_file(self, content:bytes, path:str="DEFAULT"):
+        """2 write the content of a file by emalia permission
+        """
+        pass
+        
+    def _action_make_request(self, http_method, http_url, http_header, http_body):
+        """3 make an external request by emalia permission
+        """
+        pass
+    
+    def _action_execute_powershell(self, command):
+        """4 Execute a powershell command by emalia permission
+        """
+        pass
+    
+    def _action_execute_python(self, python):
+        """5 Execute a python script in current process by emalia permission
+        """
+        pass
+    
+    def _store_custom_task(self, task):
+        """user can store custom tasks (nest multiple or define new)
+        """
+        
+    def _run_custom_task(self, name):
+        """run user stored custom tasks 
+        """
+    
 if __name__ == "__main__":
     #TODO support comamndline trigger of emalia_main.py
     emalia_instance = Emalia()
