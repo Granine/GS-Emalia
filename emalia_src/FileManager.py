95187e01f14f8cd4c409aec9fe67ccc168cb8a7c
Granine <guanzheng.huang@hotmail.com>
2023-05-08 21:32:51 -0700

    Added more option for search func

--- emalia_src/FileManager.py
+++ emalia_src/FileManager.py
@@ -56,23 +56,25 @@ def walk_all(path, target_type="all")->str:
                 yield os.path.join(root, file_name)
             
 
-def search_all(file_name:str, path:str=os.curdir, only_base_name:bool=True, target_type:str="all", min_size:float=-1.0, max_size:float=-1, black_list:list=[]):
+def search_all(search_string:str, path:str=os.curdir, only_base_name:bool=True, target_type:str="all", min_size:float=-1.0, max_size:float=-1, black_list:list=[], exception:bool=True)->list:
     """
     search for file or directory based on file name, walk all sub dirs, case sensitive
-    @param `file_name:str` name of file to start searching
+    @param `search_string:str` name of file to start searching
     @param `path:str` path to search
     @param `only_file_name:bool` if True, will only search for file_name in base path name, not whole path
     @param `target_type:str` all, directory/dir or file
     @param `min_size:float` minimal size of target, in bytes, -1 for no boundary
     @param `max_size:float` maximum size of target, in bytes, -1 for no boundary
+    @param `exception:bool` if True raise FileNotFoundError if not found, else just return empty
     @param `black_list:list of str` key word to ignore in search, will check full path for such words
     @return `:list` return any file where file_name is part of base name
+    @
     """
     found = False
     found_files = []
     for file in walk_all(path, target_type):
         # if name in base = good, else if user wants, check whole path for file_name, also check no black_listed word is present
-        if ((file_name in os.path.basename(file)) or ((not only_base_name) and (file_name in file))) and (not any([b_word in file for b_word in black_list])):
+        if ((search_string in os.path.basename(file)) or ((not only_base_name) and (search_string in file))) and (not any([b_word in file for b_word in black_list])):
             # if found, then check file size to + performance
             size = os.path.getsize(file)
             if (min_size <= size) or min_size<0:
@@ -80,28 +82,45 @@ def search_all(file_name:str, path:str=os.curdir, only_base_name:bool=True, targ
                     found = True
                     found_files.append(file)
     if not found:
-        raise FileNotFoundError(f"File \"{file_name}\" not found in: {os.path.realpath(path)}")
+        raise FileNotFoundError(f"File \"{search_string}\" not found in: {os.path.realpath(path)}")
     return found_files
 
-def search_exact(file_name, path=os.curdir, target_type="all"):
+def search_exact(search_string:str, path=os.curdir, target_type="all", ignore_type:bool=True, exception:bool=True)->str:
     """
-    search all. but return the only exact match result
-    @param `file_name:str` name of file to start searching
+    search all. but return the only exact match result that is right aligned
+    @param `search_string:str` name of file to start searching
     @param `path:str` path to search
     @param `target_type:str` all, directory(or dir) or file
     @param `min_size:float` minimal size of target, in bytes
     @param `max_size:float` maximum size of target, in bytes
+    @param `ignore_type:bool` if True, will only attempt to match the path before "." in found files and file_name
+    @param `exception:bool` if True raise FileNotFoundError if not found, else just return empty ""
     @return `:str` one path if exact match is made with last part of found path and the match is unique (no other file can be matched)
+    @raise FileNotFoundError if not found
     Example: "a/b/c1.txt" is found "c1.txt", "b/c1.txt", "1.txt" but not with "a/b/c" or "c.txt"
     """
     matches = []
-    for file in search_all(file_name, path, target_type=target_type):
+    for file in search_all(search_string, path, target_type=target_type, exception=exception):
         # check file_name matches the last section of file
-        if file_name == file[-len(str(file_name)):]:
-            matches.append(file)
+        if ignore_type:
+            # remove part of path after . from filename and file
+            search_string = search_string.rsplit(".", 1)[0]
+            file_ignored_type = file.rsplit(".", 1)[0]
+            if search_string == file_ignored_type[-len(str(search_string)):]:
+                matches.append(file)
+        else:
+            if search_string == file[-len(str(search_string))]:
+                matches.append(file)
+            
     if len(matches) == 1:
-       return matches[0]
+        return matches[0]
     if len(matches) == 0:   
-        raise FileNotFoundError("No file found")
+        if exception: 
+            raise FileNotFoundError("No file found")
+        else:
+            return ""
     else:
-       raise FileNotFoundError("More than 1 file found, use search_all instead")
+        if exception: 
+            raise FileNotFoundError("More than 1 file found, use search_all instead")
+        else:
+            return ""
